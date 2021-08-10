048451b322ec0bf2bd18a56ae6302a915712e115
ray-creator <71918048+ray-creator@users.noreply.github.com>
2021-08-10 14:16:29 +0800

    Update csv处理.py

--- csv处理.py
+++ csv处理.py
@@ -5,13 +5,20 @@ Created on Tue Aug 10 09:46:44 2021
 
 @author: mac
 """
-#import pandas as pd
+import openpyxl
+import pandas as pd
+import xlrd
+import csv
 
 import os
-
 fname="温州代理商出货数据0805"
 
+excel_reader=pd.ExcelFile('input/'+fname+'.xlsx')  # 指定文件
+sheet_names = excel_reader.sheet_names  # 读取文件的所有表单名，得到列表
+#df_data =  excel_reader.parse(sheet_name=sheet_names[i])  # 读取表单的内容，i是表单名的索引，等价于pd.read_excel('文件.xlsx', sheet_name=sheet_names[i])
+
 fo=open('input/'+fname+'.csv')
+fo.name
 ls=[]
 for line in fo:
     line = line.replace("\n","")
@@ -19,11 +26,13 @@ for line in fo:
 
 fo.close()
 
+ls.insert()
+
 backname=''
 
-newname =fname+backname+'.csv'  #new file name
+newname =fname+backname  #new file name
 
-f=open('output/'+newname,'w')
+f=open('output/'+newname+'.csv','w')
 for item in ls:
     f.write(','.join(item)+'\n')
 
@@ -31,9 +40,11 @@ f.close()
 
 
 def csv_to_xlsx_pd():# csv to xls
-    csv = pd.read_csv('output/'+newname, encoding='utf-8')
-    csv.to_excel('1.xlsx', sheet_name='data')
+    csv = pd.read_csv('output/'+newname+'.csv', encoding='utf-8')
+    csv.to_excel('output/'+newname+'.xlsx',sheet_name=sheet_names[0],index=None)
 
 
-# if __name__ == '__main__':
-   #  csv_to_xlsx_pd()
+if __name__ == '__main__':
+    csv_to_xlsx_pd()
+
+
